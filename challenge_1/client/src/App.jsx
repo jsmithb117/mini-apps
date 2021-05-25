a628442f3d39123cb8e7a8675eaf5231ddd03ae0
jsmithb117 <jsmithb117@gmail.com>
2021-05-25 10:00:00 -0700

    Bare react project built.  Renders text

---
+++ challenge_1/client/src/App.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Finder from './components/Finder.jsx';
+
+ReactDOM.render(<Finder />, document.getElementById('app'));
---
+++ challenge_1/client/src/components/Finder.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Renders stuff
+    </div>
+
+  );
+};
+
+export default App;
