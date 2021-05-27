02eb2d645371342b1530c78a3c30e590018a7a09
jsmithb117 <jsmithb117@gmail.com>
2021-05-27 13:46:06 -0700

    Server complete

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client', 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'client', 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults"
+              }],
+              '@babel/preset-react'
+            ],
+          },
+        }],
+      },
+    ],
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  },
+};
