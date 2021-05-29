d0deaaab347ddda2f5f22efbbe793035547c2189
jsmithb117 <jsmithb117@gmail.com>
2021-05-29 12:35:23 -0700

    Renders graph of BitCoin prices for last 31 days

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-// });
 
 app.listen(port, () => {
   console.log('Crypto Chart server running on port: ', port);
