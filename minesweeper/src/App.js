154bab232acba0cba82fc8236f0e9cd9c4ae08f4
jsmithb117 <jsmithb117@gmail.com>
2021-08-23 13:45:51 -0700

    Fixed bug where user could mark an uncovered piece as a mine

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
     let row = event.target.className.split(' ')[2].split('w')[1];  //There is probably a better way to store/retrieve the rows and cols
     let col = event.target.className.split(' ')[3].split('l')[1];
 
-    if (rightClick) {
+    if (rightClick && !this.state.board[row][col].uncovered) {
       event.target.style.color = !event.target.style.color ? 'black' : null
       event.target.innerHTML = event.target.innerHTML === '?' ? 'M' : '?';
       this.setState((state) => {
